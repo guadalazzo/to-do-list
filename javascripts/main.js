e29a8291c9a09cb01b5567ff736576daeeb3a6c7
Guadalupe Jazmin Lazzo <guadalazzo@est.frba.utn.edu.ar>
2017-02-27 19:53:43 -0300

    checkedbox

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,12 +3,18 @@ $(function () {
 		function (e) {
 			guardar("date" , $(this).val())
 		})
+	$(".date").val(
+		cargar ("date")
+		);
 	function guardar(key,value) {
 		localStorage.setItem(key, value);
 	}
 	$(".note-input").change(
 		function(e){
-			guardar("item-" + $(this).attr("data-id"), $(this).val())
+			guardar("item-" + $(this).attr("data-id"), { 
+				content:$(this).val(),
+				checked:true}
+				)
 		})
 
 	for (var i = 1; i <= $(".note-input").length; i++) {
@@ -19,7 +25,7 @@ $(function () {
 	}
 
 	function cargar (key){
-		
+
 		return localStorage.getItem(key);
 	}
 });
