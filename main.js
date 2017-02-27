f7da7a4e457aee177a7cbbe3ea9a198d73376166
Guadalupe Jazmin Lazzo <guadalazzo@est.frba.utn.edu.ar>
2017-02-27 19:26:09 -0300

    also charges date

---
+++ main.js
@@ -0,0 +1,28 @@
+$(function () {
+	$(".date").change(
+		function (e) {
+			guardar("date" , $(this).val())
+		})
+	$(".date").val(
+		cargar ("date")
+		);
+	function guardar(key,value) {
+		localStorage.setItem(key, value);
+	}
+	$(".note-input").change(
+		function(e){
+			guardar("item-" + $(this).attr("data-id"), $(this).val())
+		})
+
+	for (var i = 1; i <= $(".note-input").length; i++) {
+		
+		$(".note-input[data-id=" + i + "]").val(
+			cargar("item-"+ i) 
+			)
+	}
+
+	function cargar (key){
+
+		return localStorage.getItem(key);
+	}
+});
